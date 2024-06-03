6d1e8a386061dda6d0d9f448d232cc00d0e28b75
marthinusswart <matt.swart@icloud.com>
2024-06-03 17:24:31 +1000

    updated to include the .h

--- Helloworld_Python.cpp
+++ Helloworld_Python.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <vector>
+#include <helloworld_lib.h>
 
 namespace py = pybind11;
 
@@ -20,9 +21,15 @@ float addList(std::vector<float> values)
   return result;
 }
 
+float addFloatsLib(float f1, float f2)
+{
+  return addFloats(f1, f2);
+}
+
 PYBIND11_MODULE(helloworld_python, module_handle)
 {
   module_handle.doc() = "I'm a docstring";
   module_handle.def("multiply", &multiply);
-  module_handle.def("addList", &addList);
+  module_handle.def("add_list", &addList);
+  module_handle.def("add_floats", &addFloatsLib);
 }
---
+++ include/helloworld_lib.h
@@ -0,0 +1,13 @@
+#ifndef HELLOWORLD_LIB_H
+#define HELLOWORLD_LIB_H
+
+#include <string>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int printHelloWorld();
+float addFloats(float f1, float f2);
+
+#endif
